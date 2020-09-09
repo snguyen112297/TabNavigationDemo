0c3eff5484ee67270d1ea1b93891c91f452e0e8d
snguyen112297 <snguyen@fandm.edu>
2020-09-09 11:39:24 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#9dab5e'}}>
+      <Text style={{color: 'white'}}>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#c8b372' }}>
+      <Text style={{color: 'white'}}>Settings!</Text>
+    </View>
+  );
+}
+
+function NewsFeedScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#050608'}}>
+      <Text style={{color: 'white'}}>NewsFeed!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home-variant'
+                : 'home-variant-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === 'Newsfeed') {
+              iconName = focused ? 'newspaper' : 'newspaper-plus'
+            }
+
+            // You can return any component that you like here!
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'green',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Newsfeed" component={NewsFeedScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+A Demo of Expo's TabNavigation
+
+Screenshots
+
+
+HomeScreen
+
+[![Screenshot-1599665749.png](https://i.postimg.cc/q7NpFn8b/Screenshot-1599665749.png)](https://postimg.cc/Nywh9KCm)
+
+
+Newsfeed
+
+[![Screenshot-1599665755.png](https://i.postimg.cc/65BtXFrG/Screenshot-1599665755.png)](https://postimg.cc/4n2rvLFJ)
+
+
+Settings
+
+[![Screenshot-1599665757.png](https://i.postimg.cc/LXdF0W45/Screenshot-1599665757.png)](https://postimg.cc/sBJLMwhC)
